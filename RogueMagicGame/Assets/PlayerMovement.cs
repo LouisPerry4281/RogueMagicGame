94d9358af470cd5437c066f24dcb948f03c36830
LouisPerry4281 <louisperry4281@gmail.com>
2025-03-18 12:34:31 +0000

    Basic Player Jump
    Player can now take to the skies! Currently he has no way to connect with the earth.

--- RogueMagicGame/Assets/PlayerMovement.cs
+++ RogueMagicGame/Assets/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 
     float inputDirection;
     [SerializeField] float movementSpeed = 1;
+    [SerializeField] float jumpForce = 1;
 
     void Start()
     {
@@ -25,10 +26,21 @@ public class PlayerMovement : MonoBehaviour
     private void TakeInput()
     {
         inputDirection = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayerJump();
+        }
     }
 
     private void MovePlayer()
     {
         rb.linearVelocity = new Vector2(inputDirection * movementSpeed, rb.linearVelocityY);
+    }
+
+    private void PlayerJump()
+    {
+        rb.linearVelocityY = 0;
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 }
